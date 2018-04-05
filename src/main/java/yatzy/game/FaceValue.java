373f87c7baa9e487f537158095d2b4adc993c381
Thomas <itmethomas@gmail.com>
2018-04-05 19:33:20 +0200

    Add enum for face values

---
+++ src/main/java/yatzy/game/FaceValue.java
@@ -0,0 +1,20 @@
+package yatzy.game;
+
+public enum FaceValue {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6);
+
+    private int value;
+
+    FaceValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
